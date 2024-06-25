c98893ba6dfc0ab07165caaa04a95b72d844b6f3
ShayYhee <faithosebi02@gmail.com>
2024-06-25 23:52:49 +0300

    update to model and app

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import numpy as np
+import pickle
 import xgboost as xgb
 
 app = Flask(__name__)
 
 # Load the trained XGBoost model
-xg_model = xgb.XGBClassifier(objective='reg:squarederror', random_state=42)
+xg_model = xgb.XGBClassifier()
 xg_model.load_model('cust_book_pred.json')
+
+# Load the saved one-hot encoder
+with open('onehot_encoder.pkl', 'rb') as f:
+    encoder = pickle.load(f)
 
 @app.route('/')
 def home():
@@ -19,15 +23,10 @@ def predict():
     route = request.form['route']
     bkOrg = request.form['bkOrg']
     flDur = float(request.form['flDur'])
-    exBagg = request.form['exBagg']
+    exBagg = int(request.form['exBagg'])  # Ensure this is an integer
     lenStay = float(request.form['lenStay'])
 
-#     features = ['Route', 'Booking_Origin', 'Flight_Duration', 'Extra_Baggage', 'Length_of_Stay']
-
-    features = ['route', 'booking_origin', 'flight_duration', 'wants_extra_baggage', 'length_of_stay']
-    
-    # Prepare the input data    
-#     input_data = [[route, bkOrg, flDur, exBagg, lenStay]]
+    # Prepare the input data
     input_data = pd.DataFrame({
         'route': [route],
         'booking_origin': [bkOrg],
@@ -35,27 +34,15 @@ def predict():
         'wants_extra_baggage': [exBagg],
         'length_of_stay': [lenStay]
     })
-    
-    for col in input_data.select_dtypes("object"):
-#     for col in input_data:
-        if isinstance(col, str):
-            input_data[0,col],_ = input_data[0,col].factorize()
-    
-    # Make sure that all values are valid
-    if -1 in input_data.values:
-        return render_template('index.html', prediction='Invalid input. Please check your categorical values.')
-    
-    # Prepare the input data
-    data = [[route, bkOrg, flDur, exBagg, lenStay]]
-    dmatrix = xgb.DMatrix(data)
 
-    # Make predictions
-#     prediction = bst.predict(dmatrix)
+    # Apply the same one-hot encoding
+    input_data_encoded = encoder.transform(input_data)
 
-#     Make a prediction
-    prediction = xg_model.predict(input_data)[0]
+    # Make a prediction
+    prediction = xg_model.predict(input_data_encoded)[0]
+    prediction_proba = xg_model.predict_proba(input_data_encoded)[0][1]  # Get probability of booking
 
-    return render_template('index.html', prediction=f'The predicted customer prediction is {prediction:.4f}')
+    return render_template('index.html', prediction=f'The predicted probability of booking is {prediction_proba:.4f}')
 
 if __name__ == '__main__':
     app.run(debug=True)
